929b4761697fdb7bbce8c48ac7af116e5f78550d
akeshava <akeshava@linkedin.biz>
2022-09-04 16:48:31 -0700

    Initial commit

---
+++ src/test/java/com/linkedin/demo/SpringbootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
