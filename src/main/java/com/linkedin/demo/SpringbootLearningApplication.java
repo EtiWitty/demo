71206faecc01d2a98df0fd829a35ef110daaa10f
akeshava <akeshava@linkedin.biz>
2022-09-05 13:24:35 -0700

    return array of students

--- src/main/java/com/linkedin/demo/SpringbootLearningApplication.java
+++ src/main/java/com/linkedin/demo/SpringbootLearningApplication.java
@@ -1,5 +1,11 @@
 package com.linkedin.demo;
 
+import com.linkedin.demo.student.Student;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +21,16 @@ public class SpringbootLearningApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+//	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+//		return String.format("Hello %s!", name);
+//	}
+	public List<Student> hello() {
+		return Arrays.asList(new Student(
+				1L,
+				"Atiyeh",
+				"atiyeh.atiyeh@gmail.com",
+				LocalDate.of(2000, Month.JANUARY, 5),
+				21
+		));
 	}
 }
