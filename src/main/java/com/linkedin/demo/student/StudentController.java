34d7ad5384f1bcb04a5f170922f44805fd84ba94
akeshava <akeshava@linkedin.biz>
2022-09-09 11:22:01 -0400

    implement delete method

--- src/main/java/com/linkedin/demo/student/StudentController.java
+++ src/main/java/com/linkedin/demo/student/StudentController.java
@@ -3,7 +3,9 @@ package com.linkedin.demo.student;
 import java.util.List;
 import com.linkedin.demo.student.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +25,18 @@ public class StudentController {
 
   @GetMapping
   public List<Student> getStudents() {
+
     return studentService.getStudents();
   }
 
   @PostMapping
   public void registerNewStudent(@RequestBody Student student) {
+
     studentService.addNewStudent(student);
+  }
+
+  @DeleteMapping(path = "{studentId}")
+  public void deleteStudent(@PathVariable ("studentId") Long studentId) {
+    studentService.deleteStudent(studentId);
   }
 }
